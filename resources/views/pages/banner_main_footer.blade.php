6e4bee1664c4c61b15308557a7fe34b028d80560
Viet Anh <vanhleg0908@gmail.com>
2022-06-02 14:01:25 +0700

    update

--- resources/views/pages/banner_main_footer.blade.php
+++ resources/views/pages/banner_main_footer.blade.php
@@ -1,8 +1,8 @@
 <section class="container mt-3">
     <div class="banner_center rounded-3" style="background-image: url('{{ asset("upload/images/banner.webp") }}')">
-        <div class="px-3 pb-5 pt-4">
-            <h3 style="color:aquamarine" class="mt-3">100% HOA QUẢ TƯƠI SẠCH</h3>
-            <h5 class="mt-1 text-white">GIAO HÀNG TẬN NƠI</h5>
+        <div class="text-white px-3 pb-5 pt-4">
+            <h3 class="mt-3">100% HOA QUẢ TƯƠI SẠCH</h3>
+            <h5 class="mt-1">GIAO HÀNG TẬN NƠI</h5>
             <button class="btn btn-danger mt-2">MUA NGAY</button>
             <span class="rounded-2">new</span>
         </div>
